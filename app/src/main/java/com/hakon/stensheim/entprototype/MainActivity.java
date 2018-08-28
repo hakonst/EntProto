05e5ab11229f02af2ae4ac85dfeb3fdb490cbb83
Håkon Stensheim <hakon.stensheim@gmail.com>
2018-08-28 21:23:56 +0200

    Again

--- app/src/main/java/com/hakon/stensheim/entprototype/MainActivity.java
+++ app/src/main/java/com/hakon/stensheim/entprototype/MainActivity.java
@@ -1,53 +1,51 @@
 package com.hakon.stensheim.entprototype;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private MapView mapView;
-    private double longitude;
-    private double latitude;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.mapView = new MapView(this);
-        this.mapView.getMapAsync();
-        this.mapView.onCreate(savedInstanceState);
-
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
     }
 
-    public void openMapView(View view) {
-        Intent intent = new Intent(this, MapsActivity.class);
-        intent.putExtra("longitude", longitude);
-        intent.putExtra("latitude", latitude);
-        startActivity(intent);
+    public void openGetHelp(View view) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        double latitude = Double.valueOf("59.9139");
+        double longitude = Double.valueOf("10.7522");
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Din posisjon"));
+
+        CameraUpdate center=
+                CameraUpdateFactory.newLatLng(new LatLng(59.9139,
+                        10.7522));
+        CameraUpdate zoom= CameraUpdateFactory.zoomTo(13);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
     }
 }
